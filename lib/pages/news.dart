94a187defcea158d907f2225233ca065efbd73ea
Ryan-Willpower <surasak.c.work@gmail.com>
2019-01-19 23:47:31 +0700

    [add] programmatically fetch news' data

--- lib/pages/news.dart
+++ lib/pages/news.dart
@@ -1,29 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-var ref = Firestore.instance.collection('news');
+// create a reference of data
+final ref = Firestore.instance.collection('news');
+// Temporary image
+// TODO: programatrically fetch news' image
+String imgUrl = 'https://firebasestorage.googleapis.com/v0/b/longdoo-9f16b.appspot.com/o/hello-world.jpg?alt=media&token=a609b0d5-0e5f-41a2-a1c2-a31d7fa04fe4';
 
-var newsPage = StreamBuilder<QuerySnapshot>(
+final newsPage = StreamBuilder<QuerySnapshot>(
   stream: ref.snapshots(),
   builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
     if (snapshot.hasError)
       return Text('Error: ${snapshot.hasError}');
     switch (snapshot.connectionState) {
-      case ConnectionState.waiting: return Text('Waiting...');
+      case ConnectionState.waiting: return Text('Waiting...'); // Loading if no data to serve
       default:
         return ListView(
+          // Symmetric padding adding of a list
+          padding: EdgeInsets.symmetric(
+            vertical: 20.0,
+            horizontal: 10.0
+          ),
+          // Bind data to list using list
           children: snapshot.data.documents.map((DocumentSnapshot document) {
             return Column(
+              // Horizontal align to left
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Container(
-                  child: Text(document['title']),
+                // ClipRRect is used to create a border radius
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.network(imgUrl),
                 ),
-                Container(
-                  child: Text(document['excerpt']),
+                // Padding under image 5px
+                Padding(
+                  padding: EdgeInsets.only(
+                    bottom: 5.0,
+                  )
                 ),
+                // Title text should be noticeable so the text is bold and large 
+                Text(
+                  document['title'],
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24.0,
+                  ),
+                ),
+                // The excerpt of the news
+                Text(document['excerpt']),
               ],
             );
-          }).toList(),
+          }).toList(), // Change iterable to a list
         );
     }
   },
