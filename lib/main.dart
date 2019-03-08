1d2097367e8127ac075e5f18c80c3bc57e42775d
Ryan-Willpower <surasak.c.work@gmail.com>
2019-03-08 22:47:50 +0700

    [add] bottom navbar

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,28 @@ class _AppSplashState extends State<AppSplash> {
 class AfterSplash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MainScreen();
+  }
+}
+
+// bottom navigation bar
+class MainScreen extends StatefulWidget {
+  MainScreen({Key key}) : super(key:key);
+
+  @override 
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  final _widgetOption  = [
+    Text('Index 1: Home'),
+    Text('Index 2: Search'),
+    Text('Index 3: Video'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Long Doo'),
@@ -53,9 +75,25 @@ class AfterSplash extends StatelessWidget {
         backgroundColor: Colors.black,
       ),
       body: Center(
-        child: Text('Hello World!'),
-      )
+        child: _widgetOption.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('HOME')),
+          BottomNavigationBarItem(icon: Icon(Icons.search), title:Text('SEARCH')),
+          BottomNavigationBarItem(icon: Icon(Icons.featured_video), title: Text('VIDEO')),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.black,
+        onTap: _onTapHandler,
+      ),
     );
+  }
+
+  void _onTapHandler(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
 
